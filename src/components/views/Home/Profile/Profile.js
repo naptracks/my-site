b42a522b4cd854909910646a0d786d125cb3d616
naptracks <cesar.martel9@gmail.com>
2021-11-03 17:54:42 +0100

    text in french ok

--- src/components/views/Home/Profile/Profile.js
+++ src/components/views/Home/Profile/Profile.js
@@ -24,13 +24,13 @@ const Profile = ({data}) => {
             </div>
             <div className={s.skillSet}>
                 <div className={s.ability}>
-                    <KeyPoint p={data.skillOne.p} white>{svg.iconSkill1}{data.skillOne.keyPoint}</KeyPoint>
+                    <KeyPoint minHeight p={data.skillOne.p} white>{svg.iconSkill1}{data.skillOne.keyPoint}</KeyPoint>
                 </div>
                 <div className={s.ability}>
-                    <KeyPoint p={data.skillTwo.p} white>{svg.iconSkill3} {data.skillTwo.keyPoint}</KeyPoint>
+                    <KeyPoint minHeight p={data.skillTwo.p} white>{svg.iconSkill3} {data.skillTwo.keyPoint}</KeyPoint>
                 </div>
                 <div className={s.ability}>
-                    <KeyPoint p={data.skillThree.p} white>{svg.iconSkill2} {data.skillThree.keyPoint}</KeyPoint>
+                    <KeyPoint minHeight p={data.skillThree.p} white>{svg.iconSkill2} {data.skillThree.keyPoint}</KeyPoint>
                 </div>
             </div>
 

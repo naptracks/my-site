cc1af43c2a7e49495ce40f9ac68ac7afb27908b0
naptracks <cesar.martel9@gmail.com>
2021-11-06 01:51:16 +0100

    no surfer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,25 +27,25 @@ export default function Home({isAvailable, projectOne, projectTwo, projectThree,
     const statusUpdated = isAvailable ? status.available : status.busy
 
     //surfer
-    const refMain = useRef();
-    const [anim, setAnim] = useState({transform: 'translateX(-200px)'})
-
-    useLayoutEffect(() => {
-        window.addEventListener('scroll', onScroll)
-        return () => window.removeEventListener('scroll', onScroll)
-    })
-    const onScroll = () => {
-
-        const top = refMain.current.getBoundingClientRect().top
-        const scrollRef = top + 3610;
-        const width = (scrollRef) * -2
-
-        if (top <= -3450 && top >= -4500) {
-            setAnim({
-                transform: `translateX(${width}px)`,
-            })
-        }
-    }
+    // const refMain = useRef();
+    // const [anim, setAnim] = useState({transform: 'translateX(-200px)'})
+    //
+    // useLayoutEffect(() => {
+    //     window.addEventListener('scroll', onScroll)
+    //     return () => window.removeEventListener('scroll', onScroll)
+    // })
+    // const onScroll = () => {
+    //
+    //     const top = refMain.current.getBoundingClientRect().top
+    //     const scrollRef = top + 3610;
+    //     const width = (scrollRef) * -2
+    //
+    //     if (top <= -3450 && top >= -4500) {
+    //         setAnim({
+    //             transform: `translateX(${width}px)`,
+    //         })
+    //     }
+    // }
 
 
     return (
@@ -58,14 +58,14 @@ export default function Home({isAvailable, projectOne, projectTwo, projectThree,
                 <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;700&display=swap"
                       rel="stylesheet"/>
             </Head>
-            <main ref={refMain}>
+            <main>
                 <Container isDark={true}>
                     <Header lang={lang} data={dataUpdated.header} status={lang === 'eng' ? statusUpdated.eng : statusUpdated.fr}/>
                 </Container>
                 <Container footer radius>
                     <Skills data={dataUpdated.skills}/>
                 </Container>
-                <Surfer anim={anim}/>
+                {/*<Surfer anim={anim}/>*/}
                 <Container footer isDark={true}>
                     <Profile data={dataUpdated.profile}/>
                 </Container>

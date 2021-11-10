5c76529fc0865be3ac9d1c17631eae3dbaa67c98
naptracks <cesar.martel9@gmail.com>
2021-11-10 18:05:36 +0100

    stacks

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -40,32 +40,27 @@ const Stack = () => {
     return (
         <div className={s.stack}>
 
-            <KeyPoint bar>frameworks<span className={color.orange}>.</span></KeyPoint>
+            <KeyPoint bar>frontend<span className={color.orange}>.</span></KeyPoint>
             <div className={s.content}>
                 {techno(reactjs.svg, reactjs.name, reactjs.href)}
                 {techno(nextjs.svg, nextjs.name, nextjs.href)}
-            </div>
-
-            <KeyPoint bar>languages<span className={color.orange}>.</span></KeyPoint>
-            <div className={s.content}>
                 {techno(js.svg, js.name, js.href)}
                 {techno(ts.svg, ts.name, ts.href)}
             </div>
-
             <KeyPoint bar>backend<span className={color.orange}>.</span></KeyPoint>
             <div className={s.content}>
                 {techno(nodejs.svg, nodejs.name, nodejs.href)}
                 {techno(graphql.svg, graphql.name, graphql.href)}
                 {techno(apollo.svg, apollo.name, apollo.href)}
+                {techno(firebase.svg, firebase.name, firebase.href)}
+                {techno(mongodb.svg, mongodb.name, mongodb.href)}
             </div>
 
             <KeyPoint bar>hosting<span className={color.orange}>.</span></KeyPoint>
             <div className={s.content}>
                 {techno(aws.svg, aws.name, aws.href)}
                 {techno(heroku.svg, heroku.name, heroku.href)}
-                {techno(firebase.svg, firebase.name, firebase.href)}
                 {techno(vercel.svg, vercel.name, vercel.href)}
-                {techno(mongodb.svg, mongodb.name, mongodb.href)}
             </div>
             <KeyPoint bar>styles<span className={color.orange}>.</span></KeyPoint>
             <div className={s.content}>
